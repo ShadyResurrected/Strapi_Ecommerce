b1393094a171c4286a02acc9d83935fd4ca7d1d1
ShadyResurrected <cchandra554@gmail.com>
2023-01-25 12:01:02 +0530

    Added strapi
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -2,22 +2,16 @@ import "./Category.scss";
 
 import cat1 from "../../../assets/category/cat-1.jpg";
 
-const Category = () => {
+const Category = ({ categories }) => {
+console.log("This from categ " + categories)
   return (
     <div className="shop-by-category">
       <div className="categories">
-        <div className="category">
-          <img src={cat1} alt="Category" />
-        </div>
-        <div className="category">
-          <img src={cat1} alt="Category" />
-        </div>
-        <div className="category">
-          <img src={cat1} alt="Category" />
-        </div>
-        <div className="category">
-          <img src={cat1} alt="Category" />
-        </div>
+        {categories.data.map((item) => (
+          <div key={item.id} className="category">
+            <img src={process.env.REACT_APP_BASE_URL + item.attributes.img.data.attributes.url} alt="Category" />
+          </div>
+        ))}
       </div>
     </div>
   );
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,43 @@
 import "./Home.scss";
+
+import { useEffect, useContext } from "react";
+
 import Banner from "./Banner/Banner";
-import Category from '../Home/Category/Category';
+import Category from "../Home/Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
+
+import { Context } from "../../utils/context";
 
 const Home = () => {
+  const { products, setProducts, categories, setCategories } = useContext(
+    Context
+  );
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
+  }, []);
+
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories?populate=*").then((res) => {
+      setCategories(res);
+    });
+  };
+  const getProducts = () => {
+    // populate=* is used to fetch all the data from strapi
+    fetchDataFromApi("/api/products?populate=*").then((res) => {
+      setProducts(res);
+    });
+  };
+
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category />
-          <Products headingText="Popular Products"/>
+          <Category categories={categories} />
+          <Products products={products} headingText="Popular Products" />
         </div>
       </div>
     </div>
